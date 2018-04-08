afde898a7b4bfb156996d1df0837bccbb9230dac
Shiva Das <30811230+shivagloria@users.noreply.github.com>
2018-04-07 21:53:37 -0700

    Update studydungeon.py

--- studydungeon.py
+++ studydungeon.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 from flask import Flask, redirect, url_for, session, request, jsonify
-from flask_oauthlib.client import OAuth
 from flask import render_template, flash, Markup
 from flask import session
 from flask_pymongo import PyMongo
@@ -20,7 +19,7 @@ app.config['MONGO_URI'] = 'mongodb://studydungeon:goblin@ds237989.mlab.com:37989
 mongo = PyMongo(app)
 
 
-app.secret_key='w98fw9ef8hwe98fhwef'
+#app.secret_key='w98fw9ef8hwe98fhwef'
 
 #mongo.db.events.insert_one( {"Department": dept, "Class": num,
                            #  "Name": name, "Email": contact} )
