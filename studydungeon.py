cda020290dfd880f17122f84cc6bdb3a9e9250d7
Shiva Das <sgdas@ucsd.edu>
2018-04-07 20:14:23 -0700

    initial database code

---
+++ studydungeon.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template, flash, Markup
+from flask_pymongo import PyMongo
+
+app.config['MONGO_HOST'] = os.environ['MONGO_HOST']
+app.config['MONGO_PORT'] = int(os.environ['MONGO_PORT'])
+app.config['MONGO_DBNAME'] = os.environ['MONGO_DBNAME']
+app.config['MONGO_URI'] = 'mongodb://studydungeon:goblin@ds237989.mlab.com:37989/studydungeon'
+
+app = Flask(studydungeon)
+mongo = PyMongo(app)
+
+mongo.db.events.insert_one( {"Department": dept, "Class": num,
+                             "Name": name, "Email": contact} )
